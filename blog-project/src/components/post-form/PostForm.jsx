6a8cb3e8dc91ec59c1e7081ae8804c7e06c7fa6a
Muhammad Abdullah <abdulx125@gmail.com>
2024-08-09 18:52:26 +0500

    ok done with handeling , uploading image and post  UI not design almost done in postform

--- blog-project/src/components/post-form/PostForm.jsx
+++ blog-project/src/components/post-form/PostForm.jsx
@@ -6,13 +6,45 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function PostForm() {
-    const {register, handleSubmit} = useForm({
-      defaultValues:{
-        title: ""
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      title: "",
+    },
+  });
+  const navigate = useNavigate(); // not sure why i am using user.userdata
+  const post = useSelector((state) => state.user.userData);
+
+  // now there are two thing if post exist update it and if not create new post
+
+  const onSubmit = async (data) => {
+    if (post) {
+      (await data.image[0]) ? service.uploadFile(data.image[0]) : null;
+      // new image has been uploaded but we have to delete the old image too
+      if (file) {
+        service.deleteFile(post.featuredImage);
       }
-    })
-    const navigate = useNavigate()// not sure why i am using user.userdata
-    const post = useSelector(state => state.user.userData)
+      // now deletion is done we have to update the post if user updated it
+      const updatePost = await service.updateDocument(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+      if (updatePost) navigate(`/post/${post.$id}`);
+    } else {
+      // first upload image
+      let file = await service.uploadFile(data.image[0]);
+      if (file) {
+        // if file true now create document
+        let doc = await service.createDocument({
+          ...data,
+          featuredImage: file.$id,
+          userId: userData.$id,
+        });
+        if (doc) {
+          navigate(`/post/${doc.$id}`);
+        }
+      }
+    }
+  };
   return <div>PostForm</div>;
 }
 
